96abc6524477fbe839846e185491dacadc105938
liaoyu <doitliao@126.com>
2020-04-02 13:57:59 +0800

    多条件筛选

--- futu/examples/simple.py
+++ futu/examples/simple.py
@@ -1,44 +1,99 @@
 # -*- coding: utf-8 -*-
+from pprint import pprint
+
 from futu import *
 
-
-quote_ctx = OpenQuoteContext(host='127.0.0.1', port=11111)
-
-"""-------------------------------------------------"""
-
 from futu.quote.quote_get_warrant import Request
+
 from futu.quote.quote_stockfilter_info import SimpleFilter
 
-field = SimpleFilter()
-field.filter_min = 100
-field.filter_max = 1000
-field.stock_field = StockField.CUR_PRICE
-field.is_no_filter = False
-field.sort = SortDir.ASCEND
 
-# field2 = SimpleFilter()
-# field2.filter_min = 100
-# field2.filter_max = 1000
-# field2.stock_field = StockField.VOLUME_RATIO
-# field2.sort = SortDir.ASCEND
-# field2.is_no_filter = False
-#
-# field3 = SimpleFilter()
-# field3.stock_field = StockField.CUR_PRICE_TO_HIGHEST52_WEEKS_RATIO
-# field3.is_no_filter = True
+def hk_filter(qt_ctx):
+  """-------------------------------------------------"""
 
-ret, ls = quote_ctx.get_stock_filter(Market.HK, [field])
-last_page, all_count, ret_list = ls
-print(ret_list)
+  field = SimpleFilter()
+  field.filter_min = -101
+  field.filter_max = -30
+  field.stock_field = StockField.CHANGE_RATE_BEGIN_YEAR
+  field.is_no_filter = False
+  field.sort = SortDir.ASCEND
 
-"""-------------------------------------------------"""
-# req = Request()
-# req.sort_field = SortField.CODE
-# req.ascend = True
-# req.type_list = [WrtType.BEAR, WrtType.CALL]
-# req .issuer_list = [Issuer.CS, Issuer.CT, Issuer.EA]
-# print(quote_ctx.get_warrant("HK.00700", req))
+  field2 = SimpleFilter()
+  field2.filter_min = 10
+  field2.filter_max = 1000
+  field2.stock_field = StockField.CUR_PRICE
+  field2.is_no_filter = False
+  # field2.sort = SortDir.DESCEND
 
-quote_ctx.close()
+  field3 = SimpleFilter()
+  field3.filter_min = 10000000000
+  field3.filter_max = 100000000000000
+  field3.stock_field = StockField.MARKET_VAL
+  # field3.sort = SortDir.ASCEND
+  field3.is_no_filter = False
+  #
+  # field3 = SimpleFilter()
+  # field3.stock_field = StockField.CUR_PRICE_TO_HIGHEST52_WEEKS_RATIO
+  # field3.is_no_filter = True
+
+  ret, ls = quote_ctx.get_stock_filter(Market.HK, [field, field2, field3])
+  last_page, all_count, ret_list = ls
+  pprint(all_count)
+  pprint(ret_list)
+
+  """-------------------------------------------------"""
+  return ret_list
+
+def us_filter(qt_ctx):
+  """-------------------------------------------------"""
+
+  field = SimpleFilter()
+  field.filter_min = -101
+  field.filter_max = -30
+  field.stock_field = StockField.CHANGE_RATE_BEGIN_YEAR
+  field.is_no_filter = False
+  field.sort = SortDir.ASCEND
+
+  field2 = SimpleFilter()
+  field2.filter_min = 100
+  field2.filter_max = 1000000000
+  field2.stock_field = StockField.CUR_PRICE
+  field2.is_no_filter = False
+  # field2.sort = SortDir.DESCEND
+
+  field3 = SimpleFilter()
+  field3.filter_min = 10000000000
+  field3.filter_max = 100000000000000
+  field3.stock_field = StockField.MARKET_VAL
+  # field3.sort = SortDir.ASCEND
+  field3.is_no_filter = False
+  #
+  # field3 = SimpleFilter()
+  # field3.stock_field = StockField.CUR_PRICE_TO_HIGHEST52_WEEKS_RATIO
+  # field3.is_no_filter = True
+
+  ret, ls = quote_ctx.get_stock_filter(Market.US, [field, field2, field3])
+  last_page, all_count, ret_list = ls
+  pprint(all_count)
+  pprint(ret_list)
+
+  """-------------------------------------------------"""
+  return ret_list
+
+def get_warrant(qt_ctx):
+  req = Request()
+  req.sort_field = SortField.CODE
+  req.ascend = True
+  req.type_list = [WrtType.BEAR, WrtType.CALL]
+  req .issuer_list = [Issuer.CS, Issuer.CT, Issuer.EA]
+  print(quote_ctx.get_warrant("HK.00700", req))
+
+if __name__ == '__main__':
+  quote_ctx = OpenQuoteContext(host='127.0.0.1', port=11111)
+
+
+  us_filter(quote_ctx)
+
+  quote_ctx.close()
 
 
--- futu/examples/stocksell.py
+++ futu/examples/stocksell.py
@@ -64,6 +64,45 @@ def smart_sell(quote_ctx, trade_ctx, stock_code, volume, trade_env, order_type=f
             return data
 
 
+def smart_buy(quote_ctx, trade_ctx, stock_code, volume, trade_env, order_type=ft.OrderType.NORMAL):
+    """智能买入函数"""
+    lot_size = 0
+    while True:
+        if lot_size == 0:
+            ret, data = quote_ctx.get_market_snapshot(stock_code)
+            lot_size = data.iloc[0]['lot_size'] if ret == ft.RET_OK else 0
+            if ret != ft.RET_OK:
+                print("can't get lot size, retrying:".format(data))
+                continue
+            elif lot_size <= 0:
+                raise Exception('lot size error {}:{}'.format(lot_size, stock_code))
+
+        qty = int(volume / lot_size) * lot_size
+        ret, data = quote_ctx.get_order_book(stock_code)
+        if ret != ft.RET_OK:
+            print("can't get orderbook, retrying:{}".format(data))
+            continue
+
+        print(data)
+        price = data['Ask'][0][0]
+        print('smart_buy bid price is {}'.format(price))
+
+        ret, x = trd_ctx.accinfo_query(ft.TrdEnv.SIMULATE)
+        power = x['power'][0]
+        print("power {}".format(power))
+        qty = int(int(power / price) / 100 ) * 100
+
+        print("qty {}".format(qty))
+        ret, data = trade_ctx.place_order(price=price, qty=qty, code=stock_code,
+                                          trd_side=ft.TrdSide.BUY, trd_env=trade_env, order_type=order_type)
+        if ret != ft.RET_OK:
+            print('smart_sell 下单失败:{}'.format(data))
+            return None
+        else:
+            print('smart_sell 下单成功')
+            print(data)
+            return data
+
 if __name__ =="__main__":
 
     ip = '127.0.0.1'
@@ -78,11 +117,13 @@ if __name__ =="__main__":
     trd_ctx = ft.OpenHKTradeContext(ip, port)
 
     quote_ctx.subscribe(code, ft.SubType.ORDER_BOOK)
-    print("* unlock_trade:{}".format(trd_ctx.unlock_trade(unlock_pwd)))
+    if trd_env == ft.TrdEnv.REAL:
+        print("* unlock_trade:{}".format(trd_ctx.unlock_trade(unlock_pwd)))
 
-    simple_sell(quote_ctx, trd_ctx, code, 280.0, 100, trd_env, order_type)
-    smart_sell(quote_ctx, trd_ctx, code, 100, trd_env, order_type)
 
-    quote_ctx.close()
-    trd_ctx.close()
+    # simple_sell(quote_ctx, trd_ctx, code, 380.0, 100, trd_env, order_type)
+    smart_sell(quote_ctx, trd_ctx, code, 2600, trd_env, order_type)
+    # smart_buy(quote_ctx, trd_ctx, code, 1000, trd_env, order_type)
+    # quote_ctx.close()
+    # trd_ctx.close()
 
